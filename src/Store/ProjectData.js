343cee2ce05a162323fb5705d99fb88f8e4937ef
kaur-rajdeep <kaurd3535@gmail.com>
2024-05-06 10:26:44 +0530

    added group project

--- src/Store/ProjectData.js
+++ src/Store/ProjectData.js
@@ -1,12 +1,20 @@
 import ecom from "./../assets/Project/ecom.png";
 import Weather from "./../assets/Project/Weather App.png";
 import Game from "./../assets/Project/Game.png";
-import Airwe from "./../assets/Project/Airwe.png";
-
+import group from "./../assets/Project/group.jpg";
+// import Airwe from "./../assets/Project/Airwe.png";
 
 var projects = [
   {
     id: "1",
+    name: "Eternal University Dummy Website",
+    image: group,
+    description:
+      "Group Project made using Reactjs, tailwind css used github to mantain the repository.",
+    live: "https://eu-website-tau.vercel.app/",
+  },
+  {
+    id: "2",
     name: "E-commerce App",
     image: ecom,
     description:
@@ -14,7 +22,7 @@ var projects = [
     live: "https://e-commerce-app-2022.netlify.app/",
   },
   {
-    id: "2",
+    id: "3",
     name: "Weather App",
     image: Weather,
     description:
@@ -22,21 +30,21 @@ var projects = [
     live: "https://kaur-rajdeep.github.io/Weather-App/",
   },
   {
-    id: "3",
+    id: "4",
     name: "Stone-Paper-Scissors",
     image: Game,
     description:
       "Made this small Stone-Paper-Scissors game for fun using html, css and javaScript",
     live: "https://scissors-paper-stone.netlify.app/",
   },
-  {
-    id: "4",
-    name: "Airwe Website",
-    image: Airwe,
-    description:
-      "Website for Akal Institute for Rural Women Empowerment using tailwind css first time.",
-    live: "https://airwe.netlify.app/",
-  },
+  // {
+  //   id: "5",
+  //   name: "Airwe Website",
+  //   image: Airwe,
+  //   description:
+  //     "Website for Akal Institute for Rural Women Empowerment using tailwind css first time.",
+  //   live: "https://airwe.netlify.app/",
+  // },
 ];
 
 export default projects;
